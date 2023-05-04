1aa2f8a3c5372739743a39df8c38618ba4ce8b22
joey the dragon <jcastorina@gmail.com>
2023-05-03 23:34:36 -0700

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import svelte from 'rollup-plugin-svelte';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+
+export default {
+    input: 'src/main.js', // Path to your main JavaScript file
+    output: {
+        format: 'iife',
+        name: 'app',
+        file: 'public/build/bundle.js', // The output bundle file
+    },
+    plugins: [
+        svelte({}),
+        serve({
+            contentBase: 'public',
+            host: 'localhost',
+            port: 5000,
+        }),
+        livereload({ watch: 'public' }),
+        nodeResolve({
+            exportConditions: ['svelte']
+        }),
+    ],
+};
---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  let name = 'World';
+</script>
+ 
+<h1>Hello, {name}!</h1>
+
+<button on:click={() => name = 'Svelte'}>Click me</button>
